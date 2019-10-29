42923ae1ef99f1b8ad3e023dbdd66ffad73fbe78
Bastien Boutonnet <bastien.b1@gmail.com>
2019-10-29 19:58:50 +0100

    implement argument parsing

--- exciting_application.py
+++ exciting_application.py
@@ -1,5 +1,17 @@
-def generate_message():
-    return "Hello World"
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--greet", action="store_true", default=False)
+args = parser.parse_args()
+
+
+def generate_message(greet):
+    if greet:
+        start_message = "Hello"
+    else:
+        start_message = "Goodbye"
+        return f"{start_message} World"
+
 
 if __name__ == "__main__":
-    print(generate_message())
+    print(generate_message(args.greet))
