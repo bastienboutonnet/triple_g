67567356fe16c72c64668e23d4bf335d9567cd18
Bastien Boutonnet <bastien.b1@gmail.com>
2019-10-29 20:06:28 +0100

    build hello message with time

--- exciting_application.py
+++ exciting_application.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+
+
 def generate_message():
-    return "Hello World"
+    greeting_part = "Hello World"
+    time_part = datetime.now().strftime("%H:%M")
+        return f"{greeting_part}, it is {time_part}"
+
 
 if __name__ == "__main__":
     print(generate_message())
